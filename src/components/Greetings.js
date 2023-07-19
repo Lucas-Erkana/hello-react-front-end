6752be919bf53d496ca3d3eae8e29e4c4266600a
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-19 23:05:26 +0200

    More animations

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -6,7 +6,7 @@ import './Greetings.css';
 function Greetings() {
   const dispatch = useDispatch();
   const greetingsData = useSelector((state) => state.greetings.greetings);
-  const [animation, setAnimation] = useState(false);
+  const [animation, setAnimation] = useState('');
   const singleGreeting = greetingsData[Math.floor(Math.random() * greetingsData.length)];
 
   useEffect(() => {
@@ -14,19 +14,21 @@ function Greetings() {
   }, [dispatch]);
 
   const handleClick = () => {
-    setAnimation(true);
+    const animations = ['bounceRight', 'bounceLeft', 'bounceTop', 'bounceBottom'];
+    const randomAnimation = animations[Math.floor(Math.random() * animations.length)];
+    setAnimation(randomAnimation);
     dispatch(fetchGreetings());
   };
 
   useEffect(() => {
     if (animation) {
-      setTimeout(() => setAnimation(false), 1000); // reset animation after 1s
+      setTimeout(() => setAnimation(''), 1000); // reset animation after 1s
     }
   }, [animation]);
 
   return (
     <div className="center">
-      {singleGreeting && <h2 className={animation ? 'bounce' : ''}>{singleGreeting.greeting}</h2>}
+      {singleGreeting && <h2 className={animation}>{singleGreeting.greeting}</h2>}
       <button onClick={handleClick} className="greeting-btn" type="button">
         Get Greeting
       </button>
