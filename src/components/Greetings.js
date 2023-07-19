fd1e92b01d2e89cff381211b88b5798724e4249d
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-19 23:01:39 +0200

    Add animations

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchGreetings } from '../store/greetingsSlice';
 import './Greetings.css';
@@ -6,16 +6,28 @@ import './Greetings.css';
 function Greetings() {
   const dispatch = useDispatch();
   const greetingsData = useSelector((state) => state.greetings.greetings);
+  const [animation, setAnimation] = useState(false);
   const singleGreeting = greetingsData[Math.floor(Math.random() * greetingsData.length)];
 
   useEffect(() => {
     dispatch(fetchGreetings());
   }, [dispatch]);
 
+  const handleClick = () => {
+    setAnimation(true);
+    dispatch(fetchGreetings());
+  };
+
+  useEffect(() => {
+    if (animation) {
+      setTimeout(() => setAnimation(false), 1000); // reset animation after 1s
+    }
+  }, [animation]);
+
   return (
     <div className="center">
-      {singleGreeting && <h2>{singleGreeting.greeting}</h2>}
-      <button onClick={() => dispatch(fetchGreetings())} className="greeting-btn" type="button">
+      {singleGreeting && <h2 className={animation ? 'bounce' : ''}>{singleGreeting.greeting}</h2>}
+      <button onClick={handleClick} className="greeting-btn" type="button">
         Get Greeting
       </button>
     </div>
