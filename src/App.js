2a0e7252dab51db7ef3f730afcefaab962efa243
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-19 22:32:30 +0200

    Add final touches to the styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/function-component-definition */
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Greetings from './components/Greetings';
--- src/store/greetingsSlice.js
+++ src/store/greetingsSlice.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
