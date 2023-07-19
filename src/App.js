93436da55ac20f2daf91b6cdef22f6bf51044608
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-19 20:49:40 +0200

    Set up routing

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Greetings from './components/Greetings';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Greetings />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
